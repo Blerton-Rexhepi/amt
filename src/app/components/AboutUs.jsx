7448c0b55d3b0a146fbc183ad6b7901fbf64e5ff
blerton-rexhepi <r.blerton@outlook.com>
2025-03-24 09:03:01 +0100

    feat: homepage and about us created

---
+++ src/app/components/AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function AboutUs() {
+  return (
+    <section  className="md:h-[100vh]">
+      <div className='flex items-start justify-center md:w-[50vw] flex-col text-left md:pl-[20px] m-[5px] h-[100vh]'>
+        <h1 className='pt-[10px] text-[2em]' >AMT Fighting Club </h1>
+        <p className='py-[10px]'>
+              Welcome to AMT Fighting Club, where warriors are forged and champions are made.
+              Our fight club is dedicated to the art of eight limbs, blending discipline, power,
+              and technique in every strike. Whether you're a beginner or a seasoned fighter, our experienced trainers 
+              will push you to your limits, helping you master the clinch, kicks, elbows, and punches that define Muay Thai. 
+              Step into the ring, embrace the grind, and unleash the fighter within.
+        </p>
+        <p>
+        Train hard. Fight smart. Conquer.
+        </p>
+      </div>
+    </section>
+  )
+}
---
+++ src/app/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <section>
+      
+    <div className="flex items-center justify-center w-[100%] h-[100vh] bg-black ">
+      <img src="./logo.jpg" className=" sm:w-[40vw] sm:h-[65vh] "/>
+    </div>
+     
+    </section>
+  );
+}
