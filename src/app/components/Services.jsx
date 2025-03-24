01411e6b7da4cc71fc86149deb42e287e0b9d2d8
blerton-rexhepi <r.blerton@outlook.com>
2025-03-24 09:49:20 +0100

    feat services added

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function Services() {
+  let services = [
+    { id: 1, name: "Group Training", photo: "./groupTrain.jpg" },
+    { id: 2, name: "Individual Training", photo: "./individual.jpg" },
+    { id: 3, name: "Fitnes ", photo: "./fitnes.jpg" },
+    { id: 4, name: "Dietitian", photo: "./Dietitian.jpg" },
+  ];
+
   return (
-    <section>Services</section>
-  )
+    <section className="bg-black text-white p-0 m-0">
+      <div className=" h-screen">
+        <h1 className="text-center text-[2em] pt-[50px]">Our Services</h1>
+
+        <div className=" flex gap-[20px] justify-center items-center py-[40px]">
+          {services.map((service) => (
+            <div key={service.id} className="">
+              <img
+                src={service.photo}
+                alt={service.name}
+                className="w-[300px] h-[240px]"
+              />
+              <h3 className="text-center">{service.name}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
